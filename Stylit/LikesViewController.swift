75d98923a3fdc80102444b96c6323c85bf01e2fd
Lucas <lww515@yahoo.com>
2019-02-26 19:23:22 -0800

    Modify LikesViewController

--- Stylit/LikesViewController.swift
+++ Stylit/LikesViewController.swift
@@ -105,14 +105,14 @@ class LikesViewController: UIViewController {
         
         var data: [Int] = []
         if !likesData.isEmpty {
-            for i in 1...likesData.count {
+            for i in 0..<likesData.count {
                 data.append(i)
             }
         }
         
         let dataSource = ArrayDataSource(data: data)
         let viewSource = ClosureViewSource(viewUpdater: { (view: UIView, data: Int, index: Int) in
-            let imageView = UIImageView(image: likesData[index])
+            let imageView = UIImageView(image: likesData[data])
             imageView.frame = CGRect(x: 0, y: 0, width: 150, height: 300)
             imageView.layer.masksToBounds = true
             imageView.layer.cornerRadius = 25
